f04468b13b456dbf3ec0bfb07975c1fd568788c8
jaibeer72 <jaibeer72@gmail.com>
2017-11-27 10:07:58 +0530

    See everything

--- openGl_colageTest/main.cpp
+++ openGl_colageTest/main.cpp
@@ -1,48 +1,108 @@
 #include<GL\glew.h>
 #include<glfw3.h>
-#include<iostream>
 
+#include<SOIL.h>
+#include<iostream>
 #define _USE_MATH_DEFINES
 #include<math.h>
+
 
 //GLM all headers required 
 #include<glm\glm.hpp>
 #include<glm\gtc\matrix_transform.hpp>
 #include<glm\gtc\type_ptr.hpp>
 
+int WIDTH = 720, HEIGHT = 720;
+
+
+GLuint textureID;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void drawCubes();
-typedef struct Vertex
-{
-	glm::vec3 position; 
-	glm::vec4 color;
-};
-void drawPoint(Vertex v1, GLfloat size) {
-	glPointSize(size); 
-	glBegin(GL_POINTS);
-	glColor4f(v1.color.r, v1.color.g, v1.color.b, v1.color.a);
-	glVertex3f(v1.position.x, v1.position.y, v1.position.z);
-	glEnd();
-}
 
-void drawPointsDemo(int width, int height) {
-	Vertex vert;
-	GLfloat size = 5.0f;
-	for (GLfloat x = 0.0f; x <= 1.0f; x += 0.2f, size += 5)
-	{
-		vert.position = glm::vec3(x, 0.0f, 0.0f);
-		vert.color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-		drawPoint(vert, size);
-	}
-}
+bool gamePaused = false;
 
-//Global Variables 
-const int width=700; 
-const int height = 700;
+
 GLboolean locked = GL_FALSE;
 GLfloat alpha = 210.0f, beta = -70.0f, zoom = 2.0f;
-GLfloat cursorX, cursorY; 
-bool freeze; 
+GLfloat cursorX, cursorY;
+bool freeze;
+
+void drawCubes()
+{
+	//glEnable(GL_TEXTURE_2D);
+	//glBindTexture(GL_TEXTURE_2D, textureID);
+	//glBegin(GL_QUADS);
+	//glTexCoord2f(-1, -1);
+	//glVertex3f(1.0f, 1.0f, 0.0f);
+	//glTexCoord2f(0, 1);
+	//glVertex3f(-1.0f, 1.0f, 0.0f);
+	//glTexCoord2f(0, 0);
+	//glVertex3f(-1.0f, -1.0f, 0.0f);
+	//glTexCoord2f(1, 0);
+	//glVertex3f(1.0f, -1.0f, 0.0f);
+	//glEnd();
+	glPointSize(10.0f);
+	glBegin(GL_POINTS);
+	
+	//glTexCoord2f(-0.5f, -0.5f);
+	glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f,0.3f,0.8f,1.0f);
+	glVertex3f(0.5f, -0.5f, -0.5f);	glColor4f(0.2f, 0.3f, 0.8f, 1.0f);
+	glVertex3f(0.5f, 0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.8f, 1.0f);
+	glVertex3f(0.5f, 0.5f, -0.5f); glColor4f(0.1f, 0.1f, 0.8f, 1.0f);
+	glVertex3f(-0.5f, 0.5f, -0.5f); glColor4f(0.1f, 0.3f, 0.8f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.1f, 0.3f, 0.8f, 1.0f);
+	glEnd();
+	glBegin(GL_POINTS);
+	//glBegin(GL_LINES);
+	//glBegin(GL_POLYGON);
+	glVertex3f(-0.5f, -0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.4f, 1.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.1f, 1.0f);
+	glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.1f, 1.0f);
+	glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.1f, 1.0f);
+	glVertex3f(-0.5f, 0.5f, 0.5f); glColor4f(0.2f, 1.0, 0.1f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.1f, 1.0f);
+	//glEnd();
+	glBegin(GL_POINTS);
+	//glBegin(GL_LINES);
+	//glBegin(GL_POLYGON);
+	glVertex3f(-0.5f, 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(-0.5f, 0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.f, 0.1f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(-0.5f, 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.3f, 1.0f);
+	glEnd();
+	glBegin(GL_POINTS);
+	//glBegin(GL_LINES);
+	//glBegin(GL_POLYGON);
+	glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(0.5f, 0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.3f, 1.0f);
+	glVertex3f(0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.3f, 1.0f);
+	glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glEnd();
+	glBegin(GL_POINTS);
+	//glBegin(GL_LINES);
+	//glBegin(GL_POLYGON);
+	glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.1f, 0.3f, 1.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.1f, 0.3f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
+	glEnd();
+	//	glBegin(GL_LINES);
+	//glBegin(GL_POLYGON);
+	glBegin(GL_POINTS);
+	glVertex3f(-0.5f, 0.5f, -0.5f); glColor4f(1.0f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(0.5f, 0.5f, -0.5f); glColor4f(1.0f, 0.2f, 0.3f, 1.0f);
+	glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(1.0f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(1.0f, 0.3f, 0.1f, 1.0f);
+	glVertex3f(-0.5f, 0.5f, 0.5f); glColor4f(1.0f, 0.5f, 0.3f, 1.0f);
+	glVertex3f(-0.5f, 0.5f, -0.5f); glColor4f(1.0f, 0.3f, 0.1f, 1.0f);
+	glEnd();
+}
 
 // Key Callback
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -85,17 +145,17 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
 	if (button != GLFW_MOUSE_BUTTON_LEFT)
-		return;  
+		return;
 	if (action == GLFW_PRESS)
 	{
 		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 		locked = GL_TRUE;
 	}
-		else
-		{
-			locked = GL_FALSE;
-			glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-		}
+	else
+	{
+		locked = GL_FALSE;
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	}
 }
 
 void cursor_position_callback(GLFWwindow* window, double x, double y)
@@ -106,7 +166,6 @@ void cursor_position_callback(GLFWwindow* window, double x, double y)
 		alpha += (GLfloat)(x - cursorX) / 10.0f;
 		beta += (GLfloat)(y - cursorY) / 10.0f;
 	}
-	this 
 	//update the cursor position
 	cursorX = (int)x;
 	cursorY = (int)y;
@@ -118,19 +177,64 @@ void scroll_callback(GLFWwindow* window, double x, double y)
 		zoom = 0.0f;
 }
 
-int main(void) {
-	GLFWwindow* window; 
-	if (!glfwInit())exit(EXIT_FAILURE);
-	window = glfwCreateWindow(width, height, "window API", NULL, NULL); 
-	if (!glewInit())exit(EXIT_FAILURE); 
-	glfwMakeContextCurrent(window); 
-	Vertex vert;
-	vert.position = glm::vec3(0.0f, 0.0f, 0.0f);
-	vert.color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+GLuint loadTexture(const char * filename) {
+	GLuint textureID;
+	unsigned char * image;
+	int width, height;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	// Set our texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Set texture filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load, create texture and generate mipmaps
+	image = SOIL_load_image(filename, &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	return textureID;
+}
+void init()
+{
+	//GLfloat LightPos[] = { 10.0f, 10.0f, -10.0f, 1.0f };
+	//GLfloat LightAmb[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+	//GLfloat LightDif[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+	//GLfloat LightSpc[] = { 0.9f, 0.9f, 0.9f, 1.0f };
+
+	//glLightfv(GL_LIGHT1, GL_POSITION, LightPos);
+	//glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmb);
+	//glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDif);
+	//glLightfv(GL_LIGHT1, GL_SPECULAR, LightSpc);
+
+	////glLightfv(GL_LIGHT0, GL_SPECULAR, LightSpc);
+
+	////glEnable(GL_LIGHT0);
+	//glEnable(GL_LIGHT1);
+	//glShadeModel(GL_SMOOTH);
+	//glEnable(GL_LIGHTING);
+	//glEnable(GL_COLOR_MATERIAL);
+	//glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+	//GLfloat specReflection[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	//glMaterialfv(GL_FRONT, GL_SPECULAR, specReflection);
+	//glMateriali(GL_FRONT, GL_SHININESS, 56);
+
+}
+
+int main()
+{
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwMakeContextCurrent(window);
+	glViewport(0, 0, WIDTH, HEIGHT);
 	glfwSetKeyCallback(window, key_callback);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//textureID[0] = loadTexture("1.jpg");
 	//mouse button callback
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
 
@@ -142,37 +246,54 @@ int main(void) {
 
 	glEnable(GL_DEPTH_TEST);
 
+	//GLuint texID = loadTexture("./assets/platform.jpg");
 
-	while (!glfwWindowShouldClose(window))
-	{
+	do {
+
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-
-		float ratio; 
-		int w, h; 
+		init();
+		float ratio;
+		int w, h;
 		glfwGetFramebufferSize(window, &w, &h);
 		framebuffer_size_callback(window, w, h);
-		ratio = (float)w / (float)h; 
-		//glViewport(0, 0, w, h); 
-		//glClear(GL_COLOR_BUFFER_BIT); 
-		glMatrixMode(GL_MODELVIEW); 
-		glLoadIdentity(); 
+		ratio = (float)w / (float)h;
+
+
+	//	glEnable(GL_TEXTURE_2D);
+		//glEnable(GL_BLEND | GL_ALPHA_TEST);
+		//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		//glBindTexture(GL_TEXTURE_2D, texID);
+
+		glMatrixMode(GL_TEXTURE);
+		glLoadIdentity();
+		glScalef(1.0f, -1.0f, 1.0f);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
 		glTranslatef(0.0, 0.0, -2.0);
 		glRotatef(beta, 1.0, 0.0, 0.0);
 		// rotate by alpha degrees around the z-axis
 		glRotatef(alpha, 0.0, 0.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//drawPointsDemo(width, height); 
 		
 		drawCubes();
-		glfwSwapBuffers(window); 
-		glfwPollEvents(); 
+		/*glEnable(GL_NORMALIZE);
+		glBegin(GL_QUADS);*/
+		//glTexCoord2f(1.0f, 0.0f); glVertex3f(0.18f, -0.18f, 0.0f);  // Top Left Of The Texture and Quad
+		//glTexCoord2f(1.0f, 1.0f); glVertex3f(0.18f, 0.18f, 0.0f); // Top Right Of The Texture and Quad
+		//glTexCoord2f(0.0f, 1.0f); glVertex3f(-0.18f, 0.18f, 0.0f); // Bottom Right Of The Texture and Quad
+		//glTexCoord2f(0.0f, 0.0f); glVertex3f(-0.18f, -0.18f, 0.0f);   // Bottom Left Of The Texture and Quad
 
-	}
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
+
+
+		//glEnd();
+
+		//glBindTexture(GL_TEXTURE_2D, 0);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} while (!glfwWindowShouldClose(window));
+	return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -197,67 +318,4 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	//Create the projection matrix based on the near clipping 
 	//plane and the location of the corners
 	glFrustum(-width_f, width_f, -height_f, height_f, front, back);
-}
-
-void drawCubes()
-{
-	Vertex v;
-	glBegin(GL_TRIANGLES);
-		glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f,0.3f,0.8f,1.0f);
-		glVertex3f(0.5f, -0.5f, -0.5f);	glColor4f(0.2f, 0.3f, 0.8f, 1.0f);
-		glVertex3f(0.5f, 0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.8f, 1.0f);
-		glVertex3f(0.5f, 0.5f, -0.5f); glColor4f(0.1f, 0.1f, 0.8f, 1.0f);
-		glVertex3f(-0.5f,0.5f, -0.5f); glColor4f(0.1f, 0.3f, 0.8f, 1.0f);
-		glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.1f, 0.3f, 0.8f, 1.0f);
-		glEnd();
-		glBegin(GL_TRIANGLES);
-		//glBegin(GL_LINES);
-		//glBegin(GL_POLYGON);
-		glVertex3f(-0.5f, -0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.4f, 1.0f);
-		glVertex3f(0.5f, -0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.1f, 1.0f);
-		glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.1f, 1.0f);
-		glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.1f, 1.0f);
-		glVertex3f(-0.5f, 0.5f, 0.5f); glColor4f(0.2f, 1.0, 0.1f, 1.0f);
-		glVertex3f(-0.5f, -0.5f, 0.5f); glColor4f(0.2f, 1.0f, 0.1f, 1.0f);
-		glEnd();
-		glBegin(GL_TRIANGLES);
-		//glBegin(GL_LINES);
-		//glBegin(GL_POLYGON);
-		glVertex3f(-0.5f, 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(-0.5f, 0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.f, 0.1f, 1.0f);
-		glVertex3f(-0.5f,- 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(-0.5f, 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.3f, 1.0f);
-		glEnd();
-		glBegin(GL_TRIANGLES);
-		//glBegin(GL_LINES);
-		//glBegin(GL_POLYGON);
-		glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(0.5f, 0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.3f, 1.0f);
-		glVertex3f(0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.3f, 1.0f);
-		glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glEnd();
-		glBegin(GL_TRIANGLES);
-		//glBegin(GL_LINES);
-		//glBegin(GL_POLYGON);
-		glVertex3f(-0.5f, -0.5f, -0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(0.5f,- 0.5f, -0.5f); glColor4f(0.2f, 0.1f, 0.3f, 1.0f);
-		glVertex3f(0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(-0.5f, -0.5f, 0.5f); glColor4f(0.2f, 0.1f, 0.3f, 1.0f);
-		glVertex3f(-0.5f, -0.5f,- 0.5f); glColor4f(0.2f, 0.3f, 0.1f, 1.0f);
-	glEnd();
-	//	glBegin(GL_LINES);
-	//glBegin(GL_POLYGON);
-	glBegin(GL_TRIANGLES);
-		glVertex3f(-0.5f, 0.5f, -0.5f); glColor4f(1.0f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(0.5f, 0.5f, -0.5f); glColor4f(1.0f, 0.2f, 0.3f, 1.0f);
-		glVertex3f(0.5f, 0.5f, 0.5f); glColor4f(1.0f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(0.5f,0.5f, 0.5f); glColor4f(1.0f, 0.3f, 0.1f, 1.0f);
-		glVertex3f(-0.5f, 0.5f, 0.5f); glColor4f(1.0f, 0.5f, 0.3f, 1.0f);
-		glVertex3f(-0.5f, 0.5f, -0.5f); glColor4f(1.0f, 0.3f, 0.1f, 1.0f);
-	glEnd();
 }
